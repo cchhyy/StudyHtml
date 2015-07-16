161e474733e8423952a81fa60d9174d47ab566d9
Chenuan <123456789@qq.com>
2015-07-16 11:56:00 +0800

    Initial Commit

---
+++ StudyHtml/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyHtml
+//
+//  Created by ccyy on 15/7/16.
+//  Copyright (c) 2015年 ccyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
